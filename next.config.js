3892edbaed345deb72f33189c4d73d33beac92f8
Davies Wabuluka <dwabuluka@gmail.com>
2025-12-23 08:33:42 -0600

    update

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/portfolio",
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
 };
 
 module.exports = nextConfig;
